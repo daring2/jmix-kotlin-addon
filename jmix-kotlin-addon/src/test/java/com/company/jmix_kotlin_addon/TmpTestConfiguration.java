cb71fa59087a9e34eb7ff678f26a8bb1e1ba26ef
daring <danila.daring@gmail.com>
2022-02-15 18:46:28 +0300

    init project

---
+++ jmix-kotlin-addon/src/test/java/com/company/jmix_kotlin_addon/TmpTestConfiguration.java
@@ -0,0 +1,28 @@
+package com.company.jmix_kotlin_addon;
+
+import io.jmix.core.annotation.JmixModule;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+
+@SpringBootConfiguration
+@EnableAutoConfiguration
+@Import(TmpConfiguration.class)
+@JmixModule(id = "com.company.jmix_kotlin_addon.test", dependsOn = TmpConfiguration.class)
+public class TmpTestConfiguration {
+
+    @Bean
+    @Primary
+    DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .generateUniqueName(true)
+                .setType(EmbeddedDatabaseType.HSQL)
+                .build();
+    }
+}
